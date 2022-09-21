4f506da91744755a37d1cd0e566d3b63a6428157
tzztson <zhenghu61919@gmail.com>
2022-09-21 15:36:47 +0800

    feat: add the border of button

--- components/button.tsx
+++ components/button.tsx
@@ -3,7 +3,7 @@ interface IProps {
 }
 const Button = ({ text }: IProps) => {
     return (
-        <div className="bg-gradient-to-r from-[#1170fa] to-[#14f0ef] z-40 px-6 py-1 text-md rounded-md">
+        <div className="border-[1px] border-[#13cdf2] bg-gradient-to-r from-[#1170fa] to-[#14f0ef] z-40 px-6 py-1 text-md rounded-md">
             <span className="text-white">{text}</span>
         </div>
     )
