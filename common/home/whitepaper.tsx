a2180b516e272f82c27105e74a01238971f3a0c7
tzztson <zhenghu61919@gmail.com>
2022-11-28 09:20:45 +0800

    update: changed the georli to mainnet

--- common/home/whitepaper.tsx
+++ common/home/whitepaper.tsx
@@ -1,31 +1,7 @@
 import Button from "../../components/button";
-import axios from "axios";
 import { useState, useEffect } from "react";
 
 const Whitepaper = () => {
-  // const [BNBprice, setBNBPrice] = useState<number>(0);
-  // const [ETHprice, setETHPrice] = useState<number>(0);
-
-  // const getPrice = async () => {
-  //   var promisePrice = [];
-  //   promisePrice.push(
-  //     axios.get(
-  //       `https://api.coingecko.com/api/v3/simple/price?ids=binancecoin&vs_currencies=usd`
-  //     )
-  //   );
-  //   promisePrice.push(
-  //     axios.get(
-  //       `https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd`
-  //     )
-  //   );
-  //   const result: any = await Promise.all(promisePrice);
-  //   setBNBPrice(result[0].data.binancecoin.usd);
-  //   setETHPrice(result[1].data.ethereum.usd);
-  // };
-
-  // useEffect(() => {
-  //   getPrice();
-  // }, []);
   return (
     <div className="mt-24">
       <div className="bg-[#1d1e42] p-[32px] flex flex-col justify-center items-center gap-y-4">
--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -2,44 +2,47 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div className="mt-16 bg-[#14173c] pt-[64px] pb-[38px] flex flex-col justify-center items-center gap-5">
-      <p className="text-white text-[24px]">
-        Exchange your Crypto for LongLifeCoin{" "}
-      </p>
-      <p className="text-white text-[14px]">
-        Above Information is not guaranteed. And subject to change without
-        notice.
-      </p>
-      <div className="flex justify-center items-center gap-8 my-4">
-        <div className="border-[1px] border-[#fefefe] rounded-lg w-8  h-8 flex justify-center items-center">
-          <Image
-            src={"/assets/icons/b-facebook.png"}
-            width={18}
-            height={18}
-            alt=""
-          />
+    <div className="bg-[#14173c] px-4">
+      <div className="container mx-auto py-16 gap-3 flex flex-col justify-center items-center">
+        <p className="font-bold text-2xl">
+          Exchange your Crypto for LongLifeCoin{" "}
+        </p>
+        <p className="text-lg font-semibold">
+          Above Information is not guaranteed. And subject to change without
+          notice.
+        </p>
+        <div className="flex items-center justify-center gap-x-8 w-full">
+          <div className="py-2 px-3  border-[1px] rounded-md">
+            <Image
+              src={"/assets/icons/b-facebook.png"}
+              width={18}
+              height={18}
+              alt=""
+              className=""
+            />
+          </div>
+          <div className="py-2 px-3  border-[1px] rounded-md">
+            <Image
+              src={"/assets/icons/b-twitter.png"}
+              width={18}
+              height={18}
+              alt=""
+            />
+          </div>
+          <div className="py-2 px-3  border-[1px] rounded-md">
+            <Image
+              src={"/assets/icons/b-indeed.png"}
+              width={18}
+              height={18}
+              alt=""
+            />
+          </div>
         </div>
-        <div className="border-[1px] border-[#fefefe] rounded-lg w-8  h-8 flex justify-center items-center">
-          <Image
-            src={"/assets/icons/b-twitter.png"}
-            width={18}
-            height={18}
-            alt=""
-          />
-        </div>
-        <div className="border-[1px] border-[#fefefe] rounded-lg w-8  h-8 flex justify-center items-center">
-          <Image
-            src={"/assets/icons/b-indeed.png"}
-            width={18}
-            height={18}
-            alt=""
-          />
-        </div>
+        <hr className="w-full" />
+        <p className="text-lg font-semibold">
+          ©2022 LongLife Coin - Cryptocurrency ICO System
+        </p>
       </div>
-      <hr className="w-4/5" />
-      <p className="text-white text-[16px]">
-        ©2022 LongLife Coin - Cryptocurrency ICO System
-      </p>
     </div>
   );
 };
